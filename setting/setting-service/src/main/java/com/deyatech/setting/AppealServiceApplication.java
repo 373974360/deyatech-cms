5b6c2428d2759c409ce25b942d1ba7e0b47ac357
李苛 <373974360@qq.com>
2019-08-01 10:02:31 +0800

    增加模块目录

---
+++ setting/setting-service/src/main/java/com/deyatech/setting/AppealServiceApplication.java
@@ -0,0 +1,50 @@
+package com.deyatech.setting;
+
+import cn.hutool.json.JSONObject;
+import com.deyatech.common.exception.GlobalExceptionHandler;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * <p>
+ * cms设置模块微服务启动类
+ * </p>
+ *
+ * @author: lee.
+ * @since 2019-07-31 16:45
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+@MapperScan("com.deyatech.**.mapper")
+@ComponentScan("com.deyatech")
+@ServletComponentScan("com.deyatech.common.filter")
+@EnableSwagger2
+@EnableCaching
+@RestController
+public class SettingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SettingServiceApplication.class, args);
+    }
+
+    @Bean
+    GlobalExceptionHandler getGlobalExceptionHandler() {
+        return new GlobalExceptionHandler();
+    }
+
+    @GetMapping("/health")
+    public JSONObject health() {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("status", "UP");
+        return jsonObject;
+    }
+}
