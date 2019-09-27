7d2c40bf1670d993c95c4660896b1ad8ab5b2320
杨崇兴 <yangchongxing@126.com>
2019-09-27 16:29:29 +0800

    websocket 修改

---
+++ interview/interview-service/src/main/java/com/deyatech/interview/config/WebSecurityConfig.java
@@ -0,0 +1,21 @@
+package com.deyatech.interview.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @Description websocket安全配置
+ * @Author yxz
+ * @Date 2019/4/22
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    public void configure(WebSecurity web) {
+        //设置不拦截规则
+        web.ignoring().antMatchers("/**");
+    }
+}
