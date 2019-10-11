0060c2b5dd2a20cf03590c993d13a1576b46eb7b
杨崇兴 <yangchongxing@126.com>
2019-10-11 14:32:45 +0800

    用户栏目内容权限

---
+++ station/station-service/src/main/java/com/deyatech/station/mapper/TemplateUserAuthorityMapper.java
@@ -0,0 +1,16 @@
+package com.deyatech.station.mapper;
+
+import com.deyatech.station.entity.TemplateUserAuthority;
+import com.deyatech.common.base.BaseMapper;
+
+/**
+ * <p>
+ * 内容模板用户权限信息 Mapper 接口
+ * </p>
+ *
+ * @Author lee.
+ * @since 2019-10-11
+ */
+public interface TemplateUserAuthorityMapper extends BaseMapper<TemplateUserAuthority> {
+
+}
